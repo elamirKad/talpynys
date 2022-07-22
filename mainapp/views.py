12c6143809b253e75075edfc8947062c6eca6178
elamirkad <elamirkad@gmail.com>
2022-07-22 16:07:37 +0500

    Tasks page added
    Views-urls managed and tasks template created

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def tasks(request):
+    dictionary = {
+        'test': 'Test'
+    }
+    return render(request, 'tasks.html', dictionary)
