98e0acd9afb4d232dae5c4f12ec5012d1d00b9e7
elamirkad <elamirkad@gmail.com>
2022-07-22 16:32:51 +0500

    Landing App created
    Created urls-view configuration
    Landing template created

--- talpynys/urls.py
+++ talpynys/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('mainapp.urls'))
+    path('app/', include('mainapp.urls')),
+    path('', include('landing.urls'))
 ]
